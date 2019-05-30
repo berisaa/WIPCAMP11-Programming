c42556db3c07788464c0d293ba8214be2bd170bc
berisaa <chayanin.bris@gmail.com>
2019-05-30 11:37:25 +0700

    [ADD] New project

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("บรีส ซากุระ\n");
+        
+        System.out.print("ครีม\n");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.print("ใบเฟิร์น\n");
+        
+        System.out.print("ซากุระครับโผมม");
+        
+    }
+}
